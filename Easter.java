007883e03f439dc912f14b197742c395a7efb28d
imjustTired <rodwelljoshalfred@gmail.com>
2025-09-05 17:26:21 +0000

    assignment

--- Easter.java
+++ Easter.java
@@ -1,15 +1,52 @@
 // Rodwell
-// This program will calcualte the date east falls on given a year after 1583
+// This program will calcualte the date east falls on given a year after 2003
 public class Easter{
     public static void main(String[] args) {
-    int year = 1583;
-    int a = year % 19; 
+    int year = 2003;
+    int a = year % 19;
+    System.out.println("a = "+a); 
     int b = year / 100;
+    System.out.println("b = "+b);
     int c = year% 100;
+    System.out.println("c = "+c);
+    int d = b / 4;
+    System.out.println("d = "+d); 
+    int e = b % 4;
+    System.out.println("e = "+e);
+    int f = (b + 8) / 25;
+    System.out.println("f = "+f);
+    int g = (b - f + 1) / 3;
+    System.out.println("g = "+g); 
+    int h = (19 * a + b - d - g + 15) % 30;
+    System.out.println("h = "+h);
+    int i = c / 4;
+    System.out.println("i = "+i);
+    int k = c % 4;
+    System.out.println("k = "+k);
+    int r = (32 + 2 * e + 2 * i - h - k) % 7;
+    System.out.println("r = "+r);
+    int m = (a + 11 * h + 22 * r) / 451;
+    System.out.println("m = "+m); 
+    int n = (h + r - 7 * m + 114) / 31;
+    System.out.println("n = "+n);
+    int p = (h + r - 7 * m + 114) % 31;
+    System.out.println("p = "+p);
+    int day = p + 1; 
+    if (n == 4){
+        System.out.println("Easter fell on April " + day);
 
+    }   
+    if (n == 3){
+        System.out.println("Easter fell on March " + day);
+
+    }
 
 
 
 
     }
+
+
+
+
 }
